23418426c1e0486d2a565bf2e448250e2008c97d
BManZ07 <68473641+BManZ07@users.noreply.github.com>
2021-02-02 15:40:55 +1100

    init

---
+++ config.lua
@@ -0,0 +1,7 @@
+Config = {}
+
+Config.WindowTintRank = 5 -- Sergeant +
+
+Config.RequiredJob = 'police'
+
+Config.MarkerCoords = {x = 435.67633056641, y = -975.93383789063, z = 24.69979095459, h = 90.851165771484}
